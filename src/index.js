db022939e2483bfc05f2c30dc30820bc4c5812a8
Varun Jindal <vjindal0112@gmail.com>
2020-08-18 23:57:01 -0400

    got multiple pages working

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import App from "./App";
+import Home from "./Home";
+import Submitted from "./Submitted";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/home" component={Home} />
+      <Route path="/submitted" component={Submitted} />
+    </Switch>
+  </Router>
 );
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
