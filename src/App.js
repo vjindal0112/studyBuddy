74f0d3dee32058f2fa02a25d07faec77ea84c237
formans <formans@umich.edu>
2020-08-21 13:08:20 -0400

    new classes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
 import "./App.css";
-import FormPage from "./components/form1";
-import styled from "styled-components";
-import FullPage from "./components/FullPage";
 import Form from "./components/Form";
 
 const App = () => {
