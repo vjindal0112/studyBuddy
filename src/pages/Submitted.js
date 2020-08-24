f77cca614170a43eb4a0f7af2e1bdfc59450df86
Varun Jindal <vjindal0112@gmail.com>
2020-08-24 10:16:09 -0400

    seamless more classes

--- src/pages/Submitted.js
+++ src/pages/Submitted.js
@@ -9,6 +9,20 @@ const Texth4 = styled.h4`
   }
 `;
 
+const Button = styled.a`
+  border: 4px solid #ffcb05;
+  margin-top: 8px;
+  padding: 4px 8px;
+  color: #fafafa;
+  font-size: 18px;
+  background-color: rgba(0, 0, 0, 0);
+
+  transition: all 0.5s;
+  &:hover {
+    background-color: #ffcb05;
+  }
+`;
+
 const Submitted = () => {
   return (
     <>
@@ -58,6 +72,9 @@ const Submitted = () => {
         <Texth4>
           You will hear back from us via email on September 5th with your StudyBuddies. Remember to fill out the form multiple times for multiple classes.
         </Texth4>
+        <Button href="/form">
+          Another Class
+        </Button>
       </div>
     </>
   );
